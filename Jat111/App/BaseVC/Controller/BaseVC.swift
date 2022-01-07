55f3f86c603dc35d5bb28cfce9a59e248e6af4bf
Denys Andriievskyi <denys.andrievskyi@teamvoy.com>
2022-01-07 09:37:12 +0200

    Initial project setup

---
+++ Jat111/App/BaseVC/Controller/BaseVC.swift
@@ -0,0 +1,10 @@
+//
+//  BaseVC.swift
+//  Jat111
+//
+//  Created by Денис Андриевский on 07.01.2022.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {}
---
+++ Jat111/App/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  Jat111
+//
+//  Created by Денис Андриевский on 07.01.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
